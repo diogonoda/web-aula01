d80d74f1ad31779e3e173c1098c88695bdf7546c
logonrm <logonrm@fiap.com.br>
2017-06-19 20:59:03 -0300

    Commit no meio da aula

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -8,7 +8,10 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 })
 
 export class ContatoComponent implements OnInit {
+  falha = { sucesso: false, texto: "Problemas no envio do formulario de contato!" }
   contatoForm: FormGroup;
+  listaErros = [];
+  listaResultados = [];
 
   constructor(private fb: FormBuilder) { }
 
@@ -23,6 +26,7 @@ export class ContatoComponent implements OnInit {
   ngOnInit() {
      this.buildForm();
   }
+
   buildForm(){
     this.contatoForm = this.fb.group({
       'nome': ['', [Validators.required]],
@@ -30,7 +34,33 @@ export class ContatoComponent implements OnInit {
       'texto': ['', [Validators.required, Validators.minLength(4),
                         Validators.maxLength(100)]]
     })
+
+    this.contatoForm.valueChanges.subscribe(
+      data => this.onValueChanged(data)
+    );
+    this.onValueChanged();
   }
 
   onSubmit() {}
+
+  onValueChanged(data?: any) {
+    if(!this.contatoForm) return;
+
+    this.listaErros = [];
+
+    for(const field in this.contatoForm.controls){
+      var control = this.contatoForm.get(field);
+
+      if(control && control.dirty && !control.valid){
+        for(const error in control.errors) {
+          this.listaErros.push(
+            {
+              sucesso: false,
+              texto: this.mensagensErro[field][error]
+            }
+          );
+        }
+      }
+    }
+  }
 }
